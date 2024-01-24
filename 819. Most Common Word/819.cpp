7a1037a9dc3ee90a34171502d52802c5264cbecc
HaoYu Chuang <ss111062692@gapp.nthu.edu.tw>
2024-01-24 12:08:47 +0800

    update

---
+++ 819. Most Common Word/819.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+using namespace std;
+
+class Solution {
+public:
+    string mostCommonWord(string paragraph, vector<string>& banned) {
+        string str, result;
+        unordered_map<string, int> freq;
+        unordered_set<string> banned_set(banned.begin(), banned.end());
+
+        for(auto &c :paragraph)
+            c = ispunct(c) ? ' ' : tolower(c);
+        istringstream ss(paragraph);
+        while(ss>>str)
+            if(banned_set.count(str)==0 && freq[result]<++freq[str]) 
+                result = str;
+        return result;
+    }
+};
+
+int main(){
+    string paragraph = "Bob hit a ball, the hit BALL flew far after it was hit.";
+    vector<string> banned = {"hit"};
+    Solution sol;
+    cout << sol.mostCommonWord(paragraph, banned) << endl;
+    return 0;
+}
+
+// compile: g++ 819.cpp -o 819 -std=c++11
+// execute: ./819

71aa32d7dd5996d45137baf747971b98a2a1c0ce
HaoYu Chuang <ss111062692@gapp.nthu.edu.tw>
2024-04-30 01:06:15 +0800

    update 104

---
+++ easy/104. Maximum Depth of Binary Tree/104.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution{
+    public:
+        int maxDepth(TreeNode* root);
+};
+
+int Solution::maxDepth(TreeNode* root){
+    if(root == NULL)
+        return 0;
+    int l = maxDepth(root->left);
+    int r = maxDepth(root->right);
+    return max(l,r)+1;
+}
+
+int main(){
+    /* Build a tree
+     *      3
+     *     / \
+     *    9  20
+     *       / \
+     *      15  7
+    */
+    TreeNode* root = new TreeNode(3);
+    root->left = new TreeNode(9);
+    root->right = new TreeNode(20);
+    root->right->left = new TreeNode(15);
+    root->right->right = new TreeNode(7);
+    /* Test */
+    Solution sol;
+    cout << sol.maxDepth(root) << endl;
+    return 0;
+}
+
+//Compile: g++ -std=c++11 104.cpp -o 104
+//Run: ./104
---
+++ easy/104. Maximum Depth of Binary Tree/README.md
@@ -0,0 +1,17 @@
+## 104. Maximum Depth of Binary Tree
+[leetcode](https://leetcode.com/problems/maximum-depth-of-binary-tree/description/)
+
+給定一棵 binary tree's root，回傳該 binary tree's maximum depth
+
+Example
+- Input: root = [3,9,20,null,null,15,7]
+```
+    3
+   / \
+  9  20
+    /  \
+   15   7
+```
+- Output: 3
+### .cpp
+利用遞迴解，整顆 binary tree's  maximum depth 其實就是 root 左子樹的 maximum depth 和 root 右子樹的 maximum depth，取較大者再加一。

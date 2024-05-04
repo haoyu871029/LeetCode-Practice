433a7ca5b3c81b6157417ad253d512b1d0dacc5b
HaoYu Chuang <ss111062692@gapp.nthu.edu.tw>
2024-05-04 15:44:59 +0800

    update 238

---
+++ medium/238. Product of Array Except Self/238.cpp
@@ -0,0 +1,36 @@
+#include <vector>
+#include <iostream>
+#include <numeric>
+using namespace std;
+
+vector<int> productExceptSelf(vector<int>& nums) {
+    int n = nums.size();
+    vector<int> pre(n), suf(n), ans(n);
+    /* Part 1 */
+    partial_sum(nums.begin(), nums.end(), pre.begin(), multiplies<int>());
+    partial_sum(nums.rbegin(), nums.rend(), suf.rbegin(), multiplies<int>()); //想像成反過來做，反過來放。
+    /* Part 2 */
+    for(int i=0; i<n; i++){
+        /* 注意邊界元素 */
+        int pre_i = ((i==0) ? 1 : pre[i-1]);
+        int suf_i = ((i==n-1) ? 1 : suf[i+1]);
+        ans[i] = pre_i * suf_i;
+    }
+    return ans;
+}
+
+int main(){
+    vector<int> nums = {1,2,3,4};
+    vector<int> ans = productExceptSelf(nums);
+    //pre: {1, 2, 6, 24}
+    //suf: {24, 24, 12, 4}
+    for(int i=0; i<ans.size(); i++){
+        cout << ans[i] << " ";
+    }
+    cout << endl;
+    return 0;
+
+}
+
+//Compile: g++ -std=c++11 238.cpp -o 238
+//Run: ./238
---
+++ medium/238. Product of Array Except Self/README.md
@@ -0,0 +1,33 @@
+## 238. Product of Array Except Self
+
+給定一個整數陣列 nums，回傳一個整數陣列 ans，其中 ans[i] 是 nums 中除了 nums[i] 之外所有元素的乘積。限制是要在 O(n) 時間複雜度內完成，且不能使用除法運算。
+
+Example
+- Input: nums = [1,2,3,4]
+- Output: [24,12,8,6]
+- Explanation: 
+    > - ans[0] = 2*3*4 = 24
+    > - ans[1] = 1*3*4 = 12
+    > - ans[2] = 1*2*4 = 8
+    > - ans[3] = 1*2*3 = 6
+### .cpp
+這個解法分為兩部分來完成
+1. 先利用 partial_sum() 來計算前綴連乘與後綴連乘
+2. 每個 ans[i] 原則上是 pre[i-1] * suf[i+1] 的結果，但要考慮邊界元素的情況。
+
+以 nums = {1,2,3,4} 為例：
+- pre 存放前綴連乘
+    - pre[0] = 1
+    - pre[1] = 1*2
+    - pre[2] = 1*2*3
+    - pre[3] = 1*2*3*4
+- suf 存放後綴連乘
+    - suf[3] = 4
+    - suf[2] = 4*3
+    - suf[1] = 4*3*2
+    - suf[0] = 4*3*2*1
+
+for loop 內的程式碼可以簡化為以下
+```cpp
+int ans[i] = (i==0 ? 1 : pre[i-1]) * (i==nums.size()-1 ? 1 : suf[i+1]);
+```

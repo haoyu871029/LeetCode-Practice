8cb0f6f61885fede06402414100e125f533ee8b4
HaoYu Chuang <ss111062692@gapp.nthu.edu.tw>
2024-06-05 12:41:00 +0800

    update 234

---
+++ easy/234. Palindrome Linked List/234.cpp
@@ -0,0 +1,77 @@
+#include <deque>
+#include <stack>
+#include <iostream>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+    public:
+        bool isPalindrome1(ListNode* head);
+        bool isPalindrome2(ListNode* head);
+};
+
+bool Solution::isPalindrome1(ListNode* head) { 
+    /* I. 計算 node 總數 */
+    stack<int> stk;
+    ListNode* tmp = head;
+    int n = 0;
+    while (tmp != nullptr){
+        n++;
+        tmp = tmp->next;
+    }
+    /* II. 將第 0 ~ (n/2)-1 個 node 的值推入 stack */
+    tmp = head;
+    for (int i=0; i<=(n/2)-1; i++){//II.
+        stk.push(tmp->val);
+        tmp = tmp->next;
+    }
+    /* III. 將剩下的 node 值和從 stack pop 出來的值作比較。 */
+    if (n%2 == 1){//代表 node 總數是奇數
+        tmp = tmp->next; //跳過最中間的元素
+    }
+    while (tmp != nullptr){//III.
+        if (tmp->val != stk.top())
+            return false;
+        stk.pop();
+        tmp = tmp->next;
+    }
+    return true;
+}
+
+
+bool Solution::isPalindrome2(ListNode* head) {
+    ListNode* temp = head;
+    deque<int> q;
+    while (temp != NULL){
+        q.push_back(temp->val);
+        temp = temp->next;
+    }
+    while (!q.empty()){
+        if (q.front() != q.back())
+            return false;
+        q.pop_front();
+        if (!q.empty())
+            q.pop_back();
+    }
+    return true;
+}
+
+int main(){
+    ListNode* head = new ListNode(1);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(2);
+    head->next->next->next = new ListNode(1);
+    Solution sol;
+    cout << sol.isPalindrome2(head) << endl;
+    return 0;
+}
+
+//Compile: g++ -std=c++11 234.cpp -o 234
+//Run: ./234
---
+++ easy/234. Palindrome Linked List/README.md
@@ -0,0 +1,44 @@
+# 234. Palindrome Linked List
+## Description & Example
+[leetcode](https://leetcode.com/problems/palindrome-linked-list/description/)
+
+Description
+```
+Given the head of a singly linked list, return true if it is a palindrome or false otherwise.
+```
+給定一個單向 linked list，判斷其是否為回文。其中 linked list 至少有一個 node，且每個 node 的值在 [0,9] 之間。
+
+Example
+```
+Input: head -> 1 -> 2 -> 2 -> 1
+Output: true
+```
+## .cpp
+### Version 1: stack 法
+```
+利用 stack 來處理，需遍歷兩次 linked list
+I. 第一次遍歷是為了計算 node 總數
+II. 第二次遍歷 part 1: 遍歷第 0 ~ (n/2)-1 個 node，將他們的值都推入 stack
+III. 第二次遍歷 part 2: 先根據 node 總數決定迴圈要從第 (n/2)+1 個還是 (n/2) 個 node 開始判斷，將這些剩下的 node 值和從 stack pop 出來的值作比較。
+這個作法的時間複雜度和空間複雜度都是 O(n)
+```
+這個作法的分析如下
+- 時間複雜度: O(n)，因為總計需要遍歷兩次 linked list
+- 空間複雜度: O(n)，因為需要一個 size 為 n/2 的 stack 來存放前半部分的 node 值
+### Version 2: deque 法
+```
+利用 deque 來處理，只需遍歷一次 linked list
+I. 遍歷一次 linked list 將所有 node 值存入 deque
+II. 遍歷一次 deque，利用 deque 的特性，每次比較頭尾的值是否相同，直到 deque 為空。
+
+這個作法的好處是不需要另外判斷 node 總數是奇數還是偶數，且想法比較直觀，程式碼也較簡潔。
+```
+這個作法的分析如下
+- 時間複雜度: O(n)
+    - I. 遍歷一次 linked list 將 node 值存入 deque，時間複雜度為 O(n)
+    - II. 遍歷一次 deque 來判斷是否為回文，時間複雜度為 O(n)
+- 空間複雜度: O(n)，因為需要一個 size 為 n 的 deque 來存放所有的 node 值，故實際的額外記憶體空間會較 Version 1 多。
+
+I. 遍歷一次 linked list 將所有 node 值存入 deque
+
+II. 遍歷一次 deque，利用 deque 的特性，每次比較頭尾的值是否相同，直到 deque 為空。

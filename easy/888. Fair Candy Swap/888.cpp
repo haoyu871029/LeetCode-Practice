3139d918ecc45f2874079585153e2f397be174d9
HaoYu Chuang <ss111062692@gapp.nthu.edu.tw>
2024-10-07 15:09:03 +0800

    update 888

--- easy/888. Fair Candy Swap/888.cpp
+++ easy/888. Fair Candy Swap/888.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
 #include <vector>
+#include <numeric>
+#include <unordered_set>
 using namespace std;
 
 class Solution {
 public:
-    vector<int> fairCandySwap(vector<int>& aliceSizes, vector<int>& bobSizes);
+    vector<int> fairCandySwap_0(vector<int>& aliceSizes, vector<int>& bobSizes);
+    vector<int> fairCandySwap_1(vector<int>& alice, vector<int>& bob);
 };
 
-vector<int> Solution::fairCandySwap(vector<int>& aliceSizes, vector<int>& bobSizes) {
+vector<int> Solution::fairCandySwap_0(vector<int>& aliceSizes, vector<int>& bobSizes) {
     /* Calculate of the fair division */
     int aliceSum = 0, bobSum = 0;
     for (auto i :aliceSizes){
@@ -31,11 +34,30 @@ vector<int> Solution::fairCandySwap(vector<int>& aliceSizes, vector<int>& bobSiz
     return result; //should not reach here
 }
 
+vector<int> Solution::fairCandySwap_1(vector<int>& alice, vector<int>& bob) {
+    vector<int> result;
+    int aliceSum = accumulate(alice.begin(), alice.end(), 0);
+    int bobSum = accumulate(bob.begin(), bob.end(), 0);
+    int del = (aliceSum-bobSum)/2;
+    
+    unordered_set<int> bobSet;
+    for(int i=0; i < bob.size(); i++)
+        bobSet.insert(bob[i]);
+    for(int i=0; i < alice.size(); i++) {
+        int cur = alice[i] - del;
+        if(bobSet.find(cur) != bobSet.end()) {
+            result.insert(result.end(), {alice[i], cur});
+            break;
+        }
+    }        
+    return result;
+}
+
 int main(){
     Solution sol;
     vector<int> aliceSizes = {1, 1};
     vector<int> bobSizes = {2, 2};
-    vector<int> result = sol.fairCandySwap(aliceSizes, bobSizes);
+    vector<int> result = sol.fairCandySwap_0(aliceSizes, bobSizes);
     for (auto i :result){
         cout << i << " ";
     }//expect 1 2
--- easy/888. Fair Candy Swap/README.md
+++ easy/888. Fair Candy Swap/README.md
@@ -14,6 +14,7 @@ Explanation:
 - xxx
 - xxx
 ## .cpp
+### Version 0
 **< Description >**
 
 把解題邏輯描述一遍
@@ -31,9 +32,31 @@ Explanation:
 2. xxx ?
     > 
 
-
 **< Analysis >**
 n = aliceSizes.size()  
 m = bobSizes.size()
 - Time complexity: O(n＊m)
 - Space complexity: O(1)
+### Version 1
+**< Description >**
+
+把解題邏輯描述一遍
+
+```
+像程式碼，但又像描述
+用寫程式碼的方式寫出邏輯來
+不要像源碼那麼複雜，但又要符合源碼的邏輯
+把一些太細節的部分省略掉
+```
+
+**< Discussions >**
+1. xxx ?
+    > xxx
+2. xxx ?
+    > 
+
+**< Analysis >**
+n = alice.size()  
+m = bob.size()
+- Time complexity: O(n+m)
+- Space complexity: O(m)
